d06580ca9a6c9262e564d60a8a918490e6c873e8
carlaQc <quenallatacarla@gmail.com>
2019-04-18 17:07:42 +0100

    carpeta roles

---
+++ roles/create.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app', ['title' => __('User Management')])
+
+@section('content')
+    @include('users.partials.header', ['title' => __('Add Rol')])
+
+    <div class="container-fluid mt--7">
+        <div class="row">
+            <div class="col-xl-12 order-xl-1">
+                <div class="card bg-secondary shadow">
+                    <div class="card-header bg-white border-0">
+                        <div class="row align-items-center">
+                            <div class="col-8">
+                                <h3 class="mb-0">{{ __('Role Management') }}</h3>
+                            </div>
+                            <div class="col-4 text-right">
+                                <a href="{{ route('user.index') }}" class="btn btn-sm btn-primary">{{ __('Back to list') }}</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+
+
+                        <form method="post" action="{{ route('role.store') }}" autocomplete="off">
+                           {{ csrf_field() }}
+                            
+                            <h6 class="heading-small text-muted mb-4">{{ __('Nuevo Rol') }}</h6>
+                            <div class="pl-lg-4">
+                                <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+
+                                    <label class="form-control-label" for="input-name">{{ __('Cargo') }}</label>
+                                    <input type="text" name="name" id="input-name" class="form-control form-control-alternative{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Name') }}" value="{{ old('name') }}" required autofocus>
+
+                                    @if ($errors->has('name'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-success mt-4">{{ __('Save') }}</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        @include('layouts.footers.auth')
+    </div>
+@endsection
